d34bb4161a86ea15043b007cf83e9ccf7abbe5dc
jedi <ne280154@senshu-u.jp>
2017-10-19 13:43:38 +0900

    8章まで

--- db/schema.rb
+++ db/schema.rb
@@ -11,18 +11,29 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150108014703) do
+ActiveRecord::Schema.define(version: 20171018142105) do
+
+  create_table "articles", force: :cascade do |t|
+    t.string   "title",                       null: false
+    t.text     "body",                        null: false
+    t.datetime "released_at",                 null: false
+    t.datetime "expired_at"
+    t.boolean  "member_only", default: false, null: false
+    t.datetime "created_at",                  null: false
+    t.datetime "updated_at",                  null: false
+  end
 
   create_table "members", force: :cascade do |t|
-    t.integer  "number",                        null: false
-    t.string   "name",                          null: false
+    t.integer  "number",                          null: false
+    t.string   "name",                            null: false
     t.string   "full_name"
     t.string   "email"
     t.date     "birthday"
-    t.integer  "gender",        default: 0,     null: false
-    t.boolean  "administrator", default: false, null: false
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.integer  "gender",          default: 0,     null: false
+    t.boolean  "administrator",   default: false, null: false
+    t.datetime "created_at",                      null: false
+    t.datetime "updated_at",                      null: false
+    t.string   "hashed_password"
   end
 
 end
